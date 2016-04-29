3386b5f94b2bfe2963e72081b45a1b181b6c74a9
Kathy Higgins <khiggins@ehsms.com>
2016-04-29 15:05:50 -0600

    initial code commit

--- Multiples/ViewController.swift
+++ Multiples/ViewController.swift
@@ -10,16 +10,105 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
+// var
+    
+    var multNum: Float = 0
+    var multResult: Float = 0
+    var pressCtr = 0
+    
+    var numEntered:Bool = false
+    var maxValueHit = false
+    
+// constant
+    
+    let maxValue: Float = 150
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
 
+//Outlets
+    
+    @IBOutlet weak var logoLbl: UIImageView!
+    @IBOutlet weak var numberInText: UITextField!
+    @IBOutlet weak var playBtn: UIButton!
+
+    @IBOutlet weak var multResultOut: UILabel!
+    @IBOutlet weak var addBtn: UIButton!
+
+    @IBAction func playBtnPressed(sender: UIButton) {
+    
+        if  numberInText.text != nil && numberInText.text != "" {
+            
+            numEntered = true
+ 
+        }
+        
+        if numEntered {
+            
+            multNum = (numberInText.text! as NSString).floatValue
+        
+            hideInputScreen()
+        
+            getResult()
+        
+        } else {
+        
+            numberInText.text = "Enter a number"
+            numEntered = false
+
+        }
+    }
+    
+    @IBAction func addBtnPressed(sender: UIButton) {
+    
+        if !maxValueHit {
+            
+    
+            getResult()
+        
+        }
+    
+    }
+    
+    func hideInputScreen() {
+        
+        logoLbl.hidden = true
+        numberInText.hidden = true
+        playBtn.hidden = true
+        
+        multResultOut.hidden = false
+        addBtn.hidden = false
+    }
+    
+    
+    func reinitializeFirstScreen () {
+        
+        logoLbl.hidden = false
+        numberInText.hidden = false
+        playBtn.hidden = false
+        
+        multResultOut.hidden = true
+        addBtn.hidden = true
+        
+    }
+    
+    func getResult() {
+    
+    
+        pressCtr += 1
+        multResult = multNum * Float(pressCtr)
+        
+        if (multResult >= maxValue) {
+            
+            maxValueHit = true
+        
+        } else {
+            
+            maxValueHit = false
+        }
+        
+        multResultOut.text = "\(multNum)" + " * " + "\(pressCtr)" + " = " + "\(multResult)"
+       
+    
+    }
 
 }
 
